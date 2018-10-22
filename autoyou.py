0516bd32608fa697c1052757f3ce3f07d815baf4
toneytang <toneytang@gmail.com>
2018-10-22 22:33:22 +1300

    autoyou.py 用来以后做顶层文件 get_video_id.py 用来解密快音视的JS加密 getdouyin.py 用来从快音视刷新取得新的视频

---
+++ autoyou.py
@@ -0,0 +1,8 @@
+#download from web to favorate
+    #
+    #record the clips name into sqlite
+#random get 30 clips per folder, 5 folders
+#create video
+    #delete the folders, put the clips into "all used" folder for next generation.
+#upload video
+    #add title, private, tag, 
